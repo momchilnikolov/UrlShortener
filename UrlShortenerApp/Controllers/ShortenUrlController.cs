e6ed94e2e58baae0393687571fd42ee2ffe0b5bb
momchilnikolov <shapermech@yahoo.com>
2020-08-12 16:57:47 -0400

    first version

---
+++ UrlShortenerApp/Controllers/ShortenUrlController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlShortenerLib;
+
+namespace UrlShortenerApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShortenUrlController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<string> Post([FromBody] string url)
+        {
+
+            var result = await UrlShortener.Shorten(WebUtility.UrlEncode(url));
+            return string.Format("result: '{0}'", result);
+        }
+    }
+}
